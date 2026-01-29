f666f7a700521ab62bb3325d167d78a6f1431598
maleyalui <maleyalui1@gmail.com>
2026-01-29 18:02:35 +0300

    Added useState to btns

--- src/BookingPage/ServiceSelection/ServiceSelection.jsx
+++ src/BookingPage/ServiceSelection/ServiceSelection.jsx
@@ -1,4 +1,63 @@
+import { useState } from "react";
+
 function ServiceSelection() {
+        //Separate state for each category  
+        //General service is always active
+    const [generalSelected,setGeneralSelected] = useState(true)
+    const [oilsSelected,setOilsSelected] = useState(false)
+    const [detailingSelected,setDetailingSelected] = useState(false)
+    const [refuellingSelected,setRefuellingSelected] = useState(false)
+
+// Determine which is active
+let activeCategory = 'General Service';
+if(oilsSelected) activeCategory= 'Oils & Fluids'
+if(detailingSelected) activeCategory= 'Car Detailing'
+if(refuellingSelected) activeCategory= 'On-site Refuelling'
+
+
+    //Sub-options for each cat
+    const Categoryoptions = {
+        'General Service':[
+        'Basic Service',
+        'Standard Service',
+        'Comprehensive Service',
+        ],
+        'Oils & Fluids': [
+      'Engine Oil Change',
+      'Brake Fluid Change',
+      'Coolant Service',
+    ],
+        'Car Detailing': [
+      'Car Wash',
+      'Interior Detailing',
+      'Full Detailing',
+    ],
+    'On-site Refuelling': [
+      'Diesel Delivery (20L)',
+      'Petrol Top-up',
+      'Emergency Fuel Service',
+    ],
+  };
+
+    //Options based on selected category
+    const  currentOptions = Categoryoptions[activeCategory] || []
+
+    //handlingclick
+    const handleButtonClick = (buttonName) => {
+        //Resets all false first
+setGeneralSelected(false);
+setOilsSelected(false);
+setDetailingSelected(false);
+setRefuellingSelected(false);
+    
+//Then, set the clicked button to true
+
+if (buttonName === 'General Service') setGeneralSelected(true);
+if (buttonName === 'Oils & Fluids') setOilsSelected(true);
+if (buttonName === 'Car Detailing') setDetailingSelected(true);
+if (buttonName === 'On-site Refuelling') setRefuellingSelected(true);
+ 
+    };
     return (
         <section className="bg-white rounded-2xl shadow-md p-6 md:p-8 border border-gray-200">
             <h3 className="text-2xl font-semibold mb-6 text-gray-900 flex items-center gap-3"
@@ -7,49 +66,48 @@ function ServiceSelection() {
 
             {/* The buttons */}
             <div className="flex flex-wrap gap-4 mb-8">
+
+                {/* General Service Button */}
                 <button
-                className="
-                bg-gray-100 text-gray-800
-                px-5 py-3 md:px-6 md:py-4
-                rounded-xl font-medium
-                hover:bg-gray-200
-                hover:outline-2
-                hover:outline-blue-500">
-                    General Service
+                onClick={()=> handleButtonClick('General Service')}
+            className="
+             px-5 py-3 md:px-6 md:py-4
+             rounded-xl font-medium
+             bg-gray-100 text-gray-800 hover:outline 
+             hover:outline-blue-700" >
+                   General Service
                 </button>
 
+            {/* Oils & Fluids button */}
                 <button
+                onClick={()=> handleButtonClick('Oils & Fluids')}
                 className="
-                bg-gray-100 text-gray-800
-                px-5 py-3 md:px-6 md:py-4
-                rounded-xl font-medium
-                hover:bg-gray-200
-                hover:outline-2
-                hover:outline-blue-500">
+             px-5 py-3 md:px-6 md:py-4
+             rounded-xl font-medium
+             bg-gray-100 text-gray-800 hover:outline 
+             hover:outline-blue-700">
                     Oils & Fluids
                 </button>
 
+                {/* Car Detailing btn */}
                 <button
-                className="
-                
-                bg-gray-100 text-gray-800
-                px-5 py-3 md:px-6 md:py-4
-                rounded-xl font-medium
-                hover:bg-gray-200
-                hover:outline-2
-                hover:outline-blue-500">
+                    onClick={()=> handleButtonClick('Car Detailing')}
+             className="
+             px-5 py-3 md:px-6 md:py-4
+             rounded-xl font-medium
+             bg-gray-100 text-gray-800 hover:outline 
+             hover:outline-blue-700">
                     Car Detailing
                 </button>
 
+            {/* On-site refuelling  */}
                 <button
-                className="
-                
-                bg-gray-100 text-gray-800
-                px-5 py-3 md:px-6 md:py-4
-                rounded-xl font-medium
-                hover:bg-gray-200
-                hover:outline-2
-                hover:outline-blue-500">
+                    onClick={()=> handleButtonClick('On-site Refuelling')}
+             className="
+             px-5 py-3 md:px-6 md:py-4
+             rounded-xl font-medium
+             bg-gray-100 text-gray-800 hover:outline 
+             hover:outline-blue-700">
                     On-site Refuelling
                 </button>
             </div>
@@ -64,34 +122,43 @@ function ServiceSelection() {
                 className="
                 w-full p-4 pr-12
                  border-gray-300
-                rounded-xl text-gray-700
-                outline:hidden
+                rounded-xl text-gray-700 
                 hover:outline-blue-500
                 text-base md:text-lg">
                     {/* 
                     In the website,the options will display when a button is
                      clicked,now oly general is default
                     */ }
-                      {/* General Service */}
+
+                  {/*    <p>General Service</p>
                     <option>Basic Service</option>
                     <option>Standard Service</option>
                     <option>Comprehensive Service</option>
                 
-                        {/* Oils & Fluids */}
+                        <p>Oils & Fluids</p> 
                     <option>Engine Oil Change</option>
                     <option>Coolant Service</option>
                     <option>Brake Fluid Change</option>
 
-                    {/* Car Detailing */}
+                     <p>Car Detailing</p>
                     <option>Car Wash</option>
                     <option>Interior Detailing</option>
                     <option>Full Detailing</option>
 
-                    {/* On-site Refuellling */}
+                    <p>On-site Refuellling</p> 
                     <option>Diesel Delivery(20L)</option>
                     <option>Petrol Top-up</option>
                     <option>Emergency Fuel Service</option>
+                                                            */}
 
+
+                        {currentOptions.map((option) => (
+                            <option 
+                            key={option}
+                            value={option}>
+                                {option}
+                            </option>
+                        ))}
                 </select>
             </div>
 
